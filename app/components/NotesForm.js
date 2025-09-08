846fa502d27aa3c3a15684e65996ae952670b201
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-08 20:52:18 -0300

    feat: note item criado e implementado

--- app/components/NotesForm.js
+++ app/components/NotesForm.js
@@ -1,33 +1,32 @@
-"use client";
-
 import { useState } from "react";
 
 export default function NotesForm({ onAddNote, nextId }) {
-    const [title, setTitle] = useState("");
-    const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
-    const onSubmit = (e) => {
-        e.preventDefault();
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-        const newNote = {
-            id: nextId,
-            title,
-            content,
-            createdAt: new Date().toLocaleTimeString(),       
+    const newNote = {
+      id: nextId,
+      title,
+      content,
+      createdAt: new Date().toLocaleTimeString(),
     };
-
 
     onAddNote(newNote);
 
     setTitle("");
     setContent("");
-    };
+  };
 
-
-return (
-    <form onSubmit={onSubmit} className="p-4 border rounded-lg shadow-sm bg-white space-y-3">
+  return (
+    <form
+      onSubmit={onSubmit}
+      className="p-4 border rounded-lg shadow-sm bg-white space-y-3"
+    >
       <h2 className="text-lg font-semibold">Nova Anotação</h2>
-      
+
       <input
         type="text"
         placeholder="Título"
--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-gray-100 h-max">
         {children}
       </body>
     </html>
